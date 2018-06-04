0bf91e42d32182e5764782a1583200d5e89304ec
Evan Dhillon <evandhillon01@gmail.com>
2018-06-04 15:05:27 -0500

    Unity Prototype
    Unity Prototype

---
+++ Unity/ARDimentiaApp/Assets/WebcamSetup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Material))]
+public class WebcamSetup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		WebCamTexture newTexture = new WebCamTexture ();
+		Renderer rend = GetComponent<Renderer>();
+		rend.material.SetTexture("_MainTex", newTexture);
+		newTexture.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
